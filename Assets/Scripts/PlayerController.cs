9cd961d3eb50b64653f5719c4810e6d0f9da0ae6
Matheus J. A. Theodoro <matheusjat@gmail.com>
2017-03-08 16:49:53 -0300

    Aula 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,36 @@ public class PlayerController : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+                activity.Call<bool>("moveTaskToBack", true);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
 	void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement);
+    }
+
+    void OnTriggerEnter2D(Collider2D colisor)
+    {
+        if (colisor.gameObject.CompareTag("Ouro"))
+        {
+            colisor.gameObject.SetActive(false);
+        }
     }
 	
 }
