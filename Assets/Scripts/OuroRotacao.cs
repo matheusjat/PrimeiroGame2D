c08d8eb9d8bf397cb3e7ca8fef0bb395a976a012
Matheus J. A. Theodoro <matheusjat@gmail.com>
2017-03-15 19:25:01 -0300

    Aula 5

---
+++ Assets/Scripts/OuroRotacao.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OuroRotacao : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,0,45 * Time.deltaTime);
+        
+	}
+}
